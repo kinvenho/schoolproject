9bd6df9d064f38427a9c19a9db1cd7ec7685e63d
Kinvenho <kinvenhodev@gmail.com>
2024-02-20 07:25:51 +0000

    New changes

--- app.js
+++ app.js
@@ -32,3 +32,25 @@ const period = document.querySelector('#period');
 calculateBtn.addEventListener('click', function(){
     console.log(incomeInput.value)
 });
+
+
+
+
+// faq
+
+document.querySelectorAll('.faq-box').forEach(box => {
+    const outer = box.querySelector('.outer');
+    const inner = box.querySelector('.inner');
+    const button = box.querySelector('.btn-faq');
+
+    button.addEventListener('click', function() {
+        box.classList.toggle('active');
+        if (box.classList.contains('active')) {
+            inner.style.display = 'block';
+            box.style.maxHeight = inner.clientHeight + outer.clientHeight + 'px';
+        } else {
+            inner.style.display = 'none';
+            box.style.maxHeight = outer.clientHeight + 'px';
+        }
+    });
+});
